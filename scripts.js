188e324a3570bcf878a31b76059e878f92348f9e
Julius Simonelli <julius.simonelli@gmail.com>
2023-03-16 21:50:21 -0700

    update

--- scripts.js
+++ scripts.js
@@ -1,32 +1,36 @@
-const words = [
-    {word: 'beach', forbiddenWords: ['ocean', 'sand', 'waves', 'vacation']},
-    // Add more words and forbidden words here...
-  ];
-  
-  const wordElement = document.getElementById('word');
-  const forbiddenWordsElement = document.getElementById('forbidden-words');
-  const nextWordButton = document.getElementById('next-word');
-  
-  function generateRandomIndex(max) {
-    return Math.floor(Math.random() * max);
-  }
-  
-  function updateWord() {
-    const randomIndex = generateRandomIndex(words.length);
-    const wordObject = words[randomIndex];
-    
-    wordElement.textContent = wordObject.word;
-  
-    forbiddenWordsElement.innerHTML = '';
-    wordObject.forbiddenWords.forEach(forbiddenWord => {
-      const listItem = document.createElement('li');
-      listItem.textContent = forbiddenWord;
-      forbiddenWordsElement.appendChild(listItem);
-    });
-  }
-  
-  nextWordButton.addEventListener('click', updateWord);
-  
-  // Initialize the game with the first word
-  updateWord();
-    
+let words = [];
+
+const wordElement = document.getElementById('word');
+const forbiddenWordsElement = document.getElementById('forbidden-words');
+const nextWordButton = document.getElementById('next-word');
+
+function generateRandomIndex(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function updateWord() {
+  const randomIndex = generateRandomIndex(words.length);
+  const wordObject = words[randomIndex];
+
+  wordElement.textContent = wordObject.word;
+
+  forbiddenWordsElement.innerHTML = '';
+  wordObject.forbiddenWords.forEach(forbiddenWord => {
+    const listItem = document.createElement('li');
+    listItem.textContent = forbiddenWord;
+    forbiddenWordsElement.appendChild(listItem);
+  });
+}
+
+nextWordButton.addEventListener('click', updateWord);
+
+// Fetch the words data from words.json and initialize the game
+fetch('words.json')
+  .then(response => response.json())
+  .then(data => {
+    words = data;
+    updateWord();
+  })
+  .catch(error => {
+    console.error('Error fetching words data:', error);
+  });
