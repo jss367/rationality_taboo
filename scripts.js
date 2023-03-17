2f250e375c1b02333d02d9ee7bbba3a9779dae31
Julius Simonelli <julius.simonelli@gmail.com>
2023-03-16 23:42:22 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,111 +1,114 @@
-const wordElement = document.getElementById("word");
-const forbiddenWordsElement = document.getElementById("forbidden-words");
-const team1ScoreElement = document.getElementById("team1-score");
-const team2ScoreElement = document.getElementById("team2-score");
-const timerElement = document.getElementById("time-remaining");
-const settingsButton = document.getElementById("settings-button");
-const settingsModal = document.getElementById("settings-modal");
-const closeModal = document.querySelector(".close");
-const timerInput = document.getElementById("timer");
-const skipPenaltyInput = document.getElementById("skip-penalty");
+document.addEventListener("DOMContentLoaded", () => {
+  const wordElement = document.getElementById("word");
+  const forbiddenWordsElement = document.getElementById("forbidden-words");
 
-let team1Score = 0;
-let team2Score = 0;
-let currentTeam = 1;
-let timeRemaining = parseInt(timerElement.textContent, 10);
-let timer;
+  const team1ScoreElement = document.getElementById("team1-score");
+  const team2ScoreElement = document.getElementById("team2-score");
+  const timerElement = document.getElementById("time-remaining");
+  const settingsButton = document.getElementById("settings-button");
+  const settingsModal = document.getElementById("settings-modal");
+  const closeModal = document.querySelector(".close");
+  const timerInput = document.getElementById("timer");
+  const skipPenaltyInput = document.getElementById("skip-penalty");
 
-async function fetchWords() {
-  const response = await fetch("words.json");
-  const wordsData = await response.json();
-  return wordsData;
-}
+  let team1Score = 0;
+  let team2Score = 0;
+  let currentTeam = 1;
+  let timeRemaining = parseInt(timerElement.textContent, 10);
+  let timer;
 
-function updateWord() {
-  const wordData = words[Math.floor(Math.random() * words.length)];
-  wordElement.textContent = wordData.word;
-  forbiddenWordsElement.innerHTML = "";
-  wordData.forbidden.forEach((forbiddenWord) => {
-    const listItem = document.createElement("li");
-    listItem.textContent = forbiddenWord;
-    forbiddenWordsElement.appendChild(listItem);
-  });
-}
-
-function updateScore() {
-  if (currentTeam === 1) {
-    team1Score++;
-    team1ScoreElement.textContent = team1Score;
-  } else {
-    team2Score++;
-    team2ScoreElement.textContent = team2Score;
+  async function fetchWords() {
+    const response = await fetch("words.json");
+    const wordsData = await response.json();
+    return wordsData;
   }
-}
 
-function deductScore() {
-  if (currentTeam === 1) {
-    team1Score--;
-    team1ScoreElement.textContent = team1Score;
-  } else {
-    team2Score--;
-    team2ScoreElement.textContent = team2Score;
+  function updateWord() {
+    const wordData = words[Math.floor(Math.random() * words.length)];
+    wordElement.textContent = wordData.word;
+    forbiddenWordsElement.innerHTML = "";
+    wordData.forbidden.forEach((forbiddenWord) => {
+      const listItem = document.createElement("li");
+      listItem.textContent = forbiddenWord;
+      forbiddenWordsElement.appendChild(listItem);
+    });
   }
-}
 
-function switchPlayer() {
-  currentTeam = currentTeam === 1 ? 2 : 1;
-}
-
-function startTimer() {
-  timerElement.textContent = timeRemaining;
-  timer = setInterval(() => {
-    timeRemaining--;
-    timerElement.textContent = timeRemaining;
-    if (timeRemaining <= 0) {
-      clearInterval(timer);
-      switchPlayer();
-      timeRemaining = parseInt(timerInput.value, 10);
-      startTimer();
+  function updateScore() {
+    if (currentTeam === 1) {
+      team1Score++;
+      team1ScoreElement.textContent = team1Score;
+    } else {
+      team2Score++;
+      team2ScoreElement.textContent = team2Score;
     }
-  }, 1000);
-}
-
-let words = [];
-fetchWords().then((data) => {
-  words = data;
-  updateWord();
-  startTimer();
-});
-
-document.getElementById("done").addEventListener("click", () => {
-  updateScore();
-  updateWord();
-});
-
-document.getElementById("skip-word").addEventListener("click", () => {
-  if (skipPenaltyInput.checked) {
-    deductScore();
   }
-  updateWord();
-});
 
-document.getElementById("switch-player").addEventListener("click", () => {
-  clearInterval(timer);
-  switchPlayer();
-  timeRemaining = parseInt(timerInput.value, 10);
-  startTimer();
-});
+  function deductScore() {
+    if (currentTeam === 1) {
+      team1Score--;
+      team1ScoreElement.textContent = team1Score;
+    } else {
+      team2Score--;
+      team2ScoreElement.textContent = team2Score;
+    }
+  }
 
-settingsButton.addEventListener("click", () => {
-  settingsModal.style.display = "block";
-});
+  function switchPlayer() {
+    currentTeam = currentTeam === 1 ? 2 : 1;
+  }
 
-closeModal.addEventListener("click", () => {
-  settingsModal.style.display = "none";
-});
+  function startTimer() {
+    timerElement.textContent = timeRemaining;
+    timer = setInterval(() => {
+      timeRemaining--;
+      timerElement.textContent = timeRemaining;
+      if (timeRemaining <= 0) {
+        clearInterval(timer);
+        switchPlayer();
+        timeRemaining = parseInt(timerInput.value, 10);
+        startTimer();
+      }
+    }, 1000);
+  }
 
-window.addEventListener("click", (event) => {
-  if (event.target === settingsModal) {
+  let words = [];
+  fetchWords().then((data) => {
+    words = data;
+    updateWord();
+    startTimer();
+  });
+
+  document.getElementById("done").addEventListener("click", () => {
+    updateScore();
+    updateWord();
+  });
+
+  document.getElementById("skip-word").addEventListener("click", () => {
+    if (skipPenaltyInput.checked) {
+      deductScore();
+    }
+    updateWord();
+  });
+
+  document.getElementById("switch-player").addEventListener("click", () => {
+    clearInterval(timer);
+    switchPlayer();
+    timeRemaining = parseInt(timerInput.value, 10);
+    startTimer();
+  });
+
+  settingsButton.addEventListener("click", () => {
+    settingsModal.style.display = "block";
+  });
+
+  closeModal.addEventListener("click", () => {
     settingsModal.style.display = "none";
-  }
+  });
+
+  window.addEventListener("click", (event) => {
+    if (event.target === settingsModal) {
+      settingsModal.style.display = "none";
+    }
+  });
 });
